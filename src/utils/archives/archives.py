2b7adebc40b7b32122d39818f3bfc80f91a54aab
wyf7685 <69091901+wyf7685@users.noreply.github.com>
2023-12-12 15:55:10 +0800

    Update archives.py

--- src/utils/archives/archives.py
+++ src/utils/archives/archives.py
@@ -2,32 +2,41 @@ import os as _os
 from subprocess import Popen, PIPE
 from pathlib import Path
 from typing import List, Optional
+import platform
 
 from ..utils import run_sync
 
+WINDOWS = platform.system() == "Windows"
 
-def _init_7zip() -> tuple[Path, Path]:
-    from base64 import b64decode
-    from .raw_7z import EXE_RAW, DLL_RAW
 
-    CACHE_PATH = Path(_os.getenv("TEMP") or "data/cache")
-    CACHE_PATH.mkdir(exist_ok=True, parents=True)
+def _init_7zip() -> str:
+    if WINDOWS:
+        from base64 import b64decode
+        from .raw_7z import EXE_RAW, DLL_RAW
 
-    exe_path = CACHE_PATH / "7z.exe"
-    dll_path = CACHE_PATH / "7z.dll"
+        CACHE_PATH = Path(_os.getenv("TEMP") or "data/cache")
+        CACHE_PATH.mkdir(exist_ok=True, parents=True)
 
-    exe_path.write_bytes(b64decode(EXE_RAW))
-    dll_path.write_bytes(b64decode(DLL_RAW))
+        exe_path = CACHE_PATH / "7z.exe"
+        dll_path = CACHE_PATH / "7z.dll"
 
-    return exe_path, dll_path
+        exe_path.write_bytes(b64decode(EXE_RAW))
+        dll_path.write_bytes(b64decode(DLL_RAW))
+    else:
+        p = Popen(["which", "7z"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, _ = p.communicate()
+        if not (exe_path := output.decode().strip()):
+            raise RuntimeError("7z not installed, please install p7zip-full first")
+
+    return str(exe_path)
 
 
 def _execute_7z(args: List[str]):
     p = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate()
-    return "Everything is Ok" in output.decode("gbk") and p.returncode == 0, err.decode(
-        "gbk"
-    )
+    return "Everything is Ok" in (
+        output.decode("gbk") if WINDOWS else output.decode()
+    ) and p.returncode == 0, (err.decode("gbk") if WINDOWS else err.decode())
 
 
 def _pack_7zip(archive: Path, root: Path, password: Optional[str] = None) -> Path:
@@ -72,8 +81,11 @@ def _pack_7zip_multipart(
     success, err = _execute_7z(args)
     if success:
         return [
-            p for p in archive.parent.iterdir()
-            if p.is_file() and p.name.startswith(archive.name) and p.name.split(".")[-1].isdigit()
+            p
+            for p in archive.parent.iterdir()
+            if p.is_file()
+            and p.name.startswith(archive.name)
+            and p.name.split(".")[-1].isdigit()
         ]
     raise RuntimeError(f"压缩文件错误: {err}")
 
@@ -85,4 +97,4 @@ def pack_7zip_multipart(
     return _pack_7zip_multipart(archive, root, volume_size, password)
 
 
-EXE_PATH, DLL_PATH = _init_7zip()
+EXE_PATH = _init_7zip()
