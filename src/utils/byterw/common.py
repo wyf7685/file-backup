8ccbe652de421f7e585291b5f1b17fd2f2ae9c5b
wyf7685 <69091901+wyf7685@users.noreply.github.com>
2024-02-04 20:51:34 +0800

    优化 `byterw`

--- src/utils/byterw/common.py
+++ src/utils/byterw/common.py
@@ -1,4 +1,3 @@
-import functools
 from datetime import datetime
 from enum import IntEnum
 from pathlib import Path
@@ -21,33 +20,25 @@ class VT(IntEnum):
     Model = 10
 
 
-@functools.cache
+__I2VT: List[VT] = [
+    VT.Int,
+    VT.Float,
+    VT.Bool,
+    VT.Str,
+    VT.Bytes,
+    VT.Dict,
+    VT.List,
+    VT.Set,
+    VT.Datetime,
+    VT.Path,
+    VT.Model,
+]
+
+
 def i2vt(i: int) -> VT:
-    match i:
-        case 0:
-            return VT.Int
-        case 1:
-            return VT.Float
-        case 2:
-            return VT.Bool
-        case 3:
-            return VT.Str
-        case 4:
-            return VT.Bytes
-        case 5:
-            return VT.Dict
-        case 6:
-            return VT.List
-        case 7:
-            return VT.Set
-        case 8:
-            return VT.Datetime
-        case 9:
-            return VT.Path
-        case 10:
-            return VT.Model
-        case _:
-            raise ValueError(f"不存在ID为 {i} 的VT")
+    if 0 <= i <= 10:
+        return __I2VT[i]
+    raise ValueError(f"不存在ID为 {i} 的VT")
 
 
 type ValidType = Union[
