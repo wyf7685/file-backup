1494da8af824ab4c9212cb740a325f973e492acf
wyf7685 <69091901+wyf7685@users.noreply.github.com>
2024-02-20 21:29:37 +0800

    封装 `crypt.py` 对 C 库的调用

--- src/utils/byterw/crypt.py
+++ src/utils/byterw/crypt.py
@@ -1,65 +1,70 @@
 import base64
 import ctypes
 import functools
-import sys
 import random
 import string
-import typing
+import sys
 from pathlib import Path
 
-LIB_NAME = f"crypt.{"dll" if sys.platform == "win32" else "so"}"
-LIB_PATH = str(Path.cwd() / "lib" / LIB_NAME)
-LIB = ctypes.CDLL(LIB_PATH)
 
-lib_encrypt = LIB.encrypt
-lib_encrypt.argtypes = [
-    ctypes.c_char_p,
-    ctypes.POINTER(ctypes.c_ubyte),
-    ctypes.c_uint64,
-]
-lib_encrypt.restype = ctypes.POINTER(ctypes.c_ubyte)
+class CLib:
+    def __init__(self):
+        LIB_NAME = f"crypt.{"dll" if sys.platform == "win32" else "so"}"
+        LIB_PATH = Path.cwd() / "lib" / LIB_NAME
+        self.LIB = ctypes.CDLL(str(LIB_PATH))
 
-lib_decrypt = LIB.decrypt
-lib_decrypt.argtypes = [
-    ctypes.c_char_p,
-    ctypes.POINTER(ctypes.c_ubyte),
-    ctypes.c_uint64,
-]
-lib_decrypt.restype = ctypes.POINTER(ctypes.c_ubyte)
+        self.lib_encrypt = self.LIB.encrypt
+        self.lib_encrypt.argtypes = [
+            ctypes.c_char_p,
+            ctypes.POINTER(ctypes.c_ubyte),
+            ctypes.c_uint64,
+        ]
+        self.lib_encrypt.restype = ctypes.POINTER(ctypes.c_ubyte)
 
-lib_free = LIB.myfree
-lib_free.argtypes = [ctypes.c_void_p]
-lib_free.restype = None
+        self.lib_decrypt = self.LIB.decrypt
+        self.lib_decrypt.argtypes = [
+            ctypes.c_char_p,
+            ctypes.POINTER(ctypes.c_ubyte),
+            ctypes.c_uint64,
+        ]
+        self.lib_decrypt.restype = ctypes.POINTER(ctypes.c_ubyte)
+
+        self.lib_free = self.LIB.myfree
+        self.lib_free.argtypes = [ctypes.c_void_p]
+        self.lib_free.restype = None
+
+    def _process(self, f: bool, charset: str, data: bytes | bytearray) -> bytes:
+        d = (ctypes.c_ubyte * len(data))(*data)
+        call = self.lib_encrypt if f else self.lib_decrypt
+        ptr = call(charset.encode("utf-8"), d, len(data))
+        result = bytes(
+            ctypes.cast(ptr, ctypes.POINTER(ctypes.c_ubyte * len(data))).contents
+        )
+        self.lib_free(ptr)
+        return result
+
+    def encrypt(self, charset: str, data: bytes | bytearray) -> bytes:
+        return self._process(True, charset, data)
+
+    def decrypt(self, charset: str, data: bytes | bytearray) -> bytes:
+        return self._process(False, charset, data)
 
 
-def _process(
-    lib_call: typing.Callable[[bytes, ctypes.Array[ctypes.c_ubyte], int], typing.Any],
-    charset: str,
-    data: bytes | bytearray,
-):
-    d = (ctypes.c_ubyte * len(data))(*data)
-    ptr = lib_call(charset.encode("utf-8"), d, len(data))
-    result = bytes(
-        ctypes.cast(ptr, ctypes.POINTER(ctypes.c_ubyte * len(data))).contents
-    )
-    lib_free(ptr)
-    return result
+CLIB = CLib()
 
 
 @functools.cache
-def get_charset(key: str | int) -> str:
+def get_charset(key: str | int | None = None) -> str:
+    if key is None:
+        key = 7685
     m = list(string.ascii_uppercase + string.ascii_lowercase + string.digits + "+/")
     random.Random(key).shuffle(m)
     return "".join(m) + "="
 
 
 def encrypt(data: bytes | bytearray, key: str | int | None = None) -> bytes:
-    b64 = base64.b64encode(data)
-    charset = get_charset(key or 7685)
-    return _process(lib_encrypt, charset, b64)
+    return CLIB.encrypt(get_charset(key), base64.b64encode(data))
 
 
 def decrypt(data: bytes | bytearray, key: str | int | None = None) -> bytes:
-    charset = get_charset(key or 7685)
-    b64 = _process(lib_decrypt, charset, data)
-    return base64.b64decode(b64)
+    return base64.b64decode(CLIB.decrypt(get_charset(key), data))
